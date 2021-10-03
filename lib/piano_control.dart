c0a67ee801f55f846daf6972f53e73b56755ebc7
joshuawade <wadej@southern.edu>
2021-10-03 11:57:08 -0400

    Rewrite piano control to use custom layout

--- lib/piano_control.dart
+++ lib/piano_control.dart
@@ -13,8 +13,6 @@ class DragInfo {
 
 typedef ValueSetter<T> = void Function(T value);
 
-// TODO: rewrite with custom layout
-
 class PianoControl extends HookWidget {
   const PianoControl({
     Key? key,
@@ -34,6 +32,8 @@ class PianoControl extends HookWidget {
     final startPixelValue = useRef(-1.0);
     final startTopKeyValue = useRef(-1.0);
     final startKeyHeightValue = useRef(-1.0);
+
+    final contentRenderBox = context.findRenderObject() as RenderBox?;
 
     return Row(
       children: [
@@ -64,50 +64,45 @@ class PianoControl extends HookWidget {
         ),
         const SizedBox(width: 1),
         Expanded(
-          child: Stack(
-            children: (() {
-              var whiteKeys = <Widget>[];
-              var blackKeys = <Widget>[];
-              double keyPosAccumulator = keyValueToPixels(
-                  keyValue: 0,
-                  keyValueAtTop: keyValueAtTop,
-                  keyHeight: keyHeight);
-              for (var i = 0; i <= 87; i++) {
-                if (getKeyType(i) == KeyType.white) {
-                  var notchType = getNotchType(i);
-                  var hasTopNotch = notchType == NotchType.above ||
-                      notchType == NotchType.both;
-
-                  whiteKeys.add(
-                    Positioned(
-                      top:
-                          keyPosAccumulator - (hasTopNotch ? keyHeight / 2 : 0),
-                      left: 0,
-                      right: 0,
-                      child: _WhiteKey(
-                        keyNumber: i,
-                        keyHeight: keyHeight,
-                      ),
-                    ),
-                  );
-                } else {
-                  blackKeys.add(
-                    Positioned(
-                      top: keyPosAccumulator,
-                      left: 0,
-                      right: 23,
-                      child: _BlackKey(
-                        keyNumber: i,
-                        keyHeight: keyHeight,
-                      ),
-                    ),
-                  );
-                }
-
-                keyPosAccumulator -= keyHeight;
+          child: ClipRect(
+            child: (() {
+              if (contentRenderBox == null) {
+                return const SizedBox();
               }
 
-              return whiteKeys + blackKeys;
+              var keyValueAtBottom =
+                  (keyValueAtTop - contentRenderBox.size.height / keyHeight)
+                      .floor();
+
+              List<int> notes = [];
+
+              for (var i = keyValueAtTop.ceil(); i >= keyValueAtBottom; i--) {
+                notes.add(i);
+              }
+
+              var noteWidgets = notes.map((note) {
+                var keyType = getKeyType(note);
+
+                Widget child;
+
+                if (keyType == KeyType.white) {
+                  child = _WhiteKey(keyHeight: keyHeight, keyNumber: note);
+                } else {
+                  child = _BlackKey(keyHeight: keyHeight, keyNumber: note);
+                }
+
+                return LayoutId(id: note, child: child);
+              }).toList();
+
+              return CustomMultiChildLayout(
+                delegate: KeyLayoutDelegate(
+                  keyHeight: keyHeight,
+                  keyValueAtTop: keyValueAtTop,
+                  notes: notes,
+                  parentHeight: contentRenderBox.size.height,
+                ),
+                children: noteWidgets,
+              );
             })(),
           ),
         ),
@@ -115,6 +110,57 @@ class PianoControl extends HookWidget {
     );
   }
 }
+
+class KeyLayoutDelegate extends MultiChildLayoutDelegate {
+  KeyLayoutDelegate({
+    required this.notes,
+    required this.keyHeight,
+    required this.keyValueAtTop,
+    required this.parentHeight,
+  });
+
+  final List<int> notes;
+  final double keyValueAtTop;
+  final double keyHeight;
+  final double parentHeight;
+
+  @override
+  void performLayout(Size size) {
+    for (var note in notes) {
+      final keyType = getKeyType(note);
+      final notchType = getNotchType(note);
+
+      var y = keyValueToPixels(
+              keyValue: note.toDouble(),
+              keyValueAtTop: keyValueAtTop,
+              keyHeight: keyHeight) -
+          keyHeight +
+          // this is why I want Dart support for Prettier
+          1;
+
+      if (keyType == KeyType.white &&
+          (notchType == NotchType.above || notchType == NotchType.both)) {
+        y -= keyHeight * 0.5;
+      }
+
+      layoutChild(
+        note,
+        BoxConstraints(maxWidth: size.width),
+      );
+      positionChild(note, Offset(0, y));
+    }
+  }
+
+  @override
+  bool shouldRelayout(covariant KeyLayoutDelegate oldDelegate) {
+    if (oldDelegate.keyHeight != keyHeight ||
+        oldDelegate.keyValueAtTop != keyValueAtTop ||
+        oldDelegate.parentHeight != parentHeight) return true;
+    return false;
+  }
+}
+
+const notchWidth = 22.0;
 
 class _WhiteKey extends HookWidget {
   const _WhiteKey({Key? key, required this.keyNumber, required this.keyHeight})
@@ -125,15 +171,18 @@ class _WhiteKey extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    var notchType = getNotchType(keyNumber);
-    double widgetHeight =
+    final notchType = getNotchType(keyNumber);
+    final widgetHeight =
         notchType == NotchType.both ? keyHeight * 2 : keyHeight * 1.5;
-    var hasTopNotch =
+    final hasTopNotch =
         notchType == NotchType.both || notchType == NotchType.above;
-    var hasBottomNotch =
+    final hasBottomNotch =
         notchType == NotchType.both || notchType == NotchType.below;
 
     // 41 / 22
+
+    // 128 here is arbitrary
+    final opacity = keyNumber < 0 || keyNumber > 128 ? 0.3 : 0.6;
 
     return GestureDetector(
         onTap: () {
@@ -154,7 +203,7 @@ class _WhiteKey extends HookWidget {
                             topLeft: Radius.circular(1),
                             bottomLeft: Radius.circular(1),
                           ),
-                          color: const Color(0xFFFFFFFF).withOpacity(0.6),
+                          color: const Color(0xFFFFFFFF).withOpacity(opacity),
                         ),
                       ),
                     ),
@@ -163,7 +212,7 @@ class _WhiteKey extends HookWidget {
                 ),
               ),
               Container(
-                width: 22,
+                width: notchWidth,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(hasTopNotch ? 1 : 0),
@@ -171,7 +220,7 @@ class _WhiteKey extends HookWidget {
                     topRight: const Radius.circular(1),
                     bottomRight: const Radius.circular(1),
                   ),
-                  color: const Color(0xFFFFFFFF).withOpacity(0.6),
+                  color: const Color(0xFFFFFFFF).withOpacity(opacity),
                 ),
               ),
             ],
@@ -195,6 +244,7 @@ class _BlackKey extends HookWidget {
         color: const Color(0xFFFFFFFF).withOpacity(0.07),
       ),
       height: keyHeight - 1,
+      margin: const EdgeInsets.only(right: notchWidth + 1),
     );
   }
 }
